8a3dd34efcf6e91bc683653b491835de24ffee38
ByteOfAllTrades <ByteOfAllTrades@protonmail.com>
2022-08-16 03:56:51 -0600

    Code Challenge Submission.

--- PlayerWebApi/Controllers/PlayerController.cs
+++ PlayerWebApi/Controllers/PlayerController.cs
@@ -4,6 +4,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PlayerWebApi.Data;
+using PlayerWebApi.Data.Entities;
 
 namespace PlayerWebApi.Controllers;
 
@@ -21,30 +22,134 @@ public class PlayerController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAllAsync()
     {
-        throw new NotImplementedException();
+        try
+        {
+            List<Player> players = await _dbContext.Players.Include(x => x.PlayerSkills).ToListAsync<Player>();
+            return (Ok(players));
+        }
+        catch(Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+       
+        //throw new NotImplementedException();
+
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetAsync()
+    public async Task<IActionResult> GetAsync(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var Results = _dbContext.Players
+              .Include(x => x.PlayerSkills)
+              .SingleOrDefaultAsync(x => x.Id == id);
+            return (Ok(Results.Result));
+        }
+        catch(Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+        
+        //throw new NotImplementedException();
     }
 
     [HttpPost]
-    public async Task<IActionResult> PostAsync()
+    public async Task<IActionResult> PostAsync(string name, string position, IEnumerable<PlayerSkill> playerSkills)
     {
-        throw new NotImplementedException();
+        
+        try
+        {
+            foreach (PlayerSkill playerSkill in playerSkills)
+            {
+                if (playerSkill.Value > 99 || playerSkill.Value < 0 ) throw new Exception("Invalid value for player skill " + playerSkill.Skill +": "+playerSkill.Value);
+            }
+            if (name == null || name == "") throw new Exception("Invalid value for name: "+name);
+            if (playerSkills.Count() <= 0) throw new Exception("PlayerSkills must have at least one entry.");
+            if (position == "defender" || position == "midfielder" || position == "forward")
+            {
+                Player player = new Player();
+                player.Name = name;
+                player.Position = position;
+                player.PlayerSkills = playerSkills;
+                await _dbContext.Players.AddAsync(player);
+                _dbContext.SaveChanges();
+                int id = player.Id;
+                var Results = await _dbContext.Players.FindAsync(id);
+                return (Ok(Results));
+            }
+            else
+            {
+                throw new Exception("Invalid value for position: " + position);
+            }
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+
+
+
+        //throw new NotImplementedException();
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutAsync()
+    public async Task<IActionResult> PutAsync(int id, string name = null, string position = null, IEnumerable<PlayerSkill> playerSkills = null)
     {
-        throw new NotImplementedException();
+        try
+        { foreach (PlayerSkill playerSkill in playerSkills)
+            {
+                if (playerSkill.Value > 99 || playerSkill.Value < 0 ) throw new Exception("Invalid value for player skill " + playerSkill.Skill +": "+playerSkill.Value);
+            }
+            if (name == null || name == "") throw new Exception("Invalid value for name: "+name);
+            if (playerSkills.Count() <= 0) throw new Exception("PlayerSkills must have at least one entry.");
+            if (position == "defender" || position == "midfielder" || position == "forward")
+            {
+                Player player = _dbContext.Players.Where(s => s.Id == id).First();
+                if (name != null)
+                {
+                    player.Name = name;
+                }
+                if (position != null)
+                {
+                    player.Position = position;
+                }
+                if (playerSkills != null)
+                {
+                    player.PlayerSkills = playerSkills;
+                }
+                _dbContext.SaveChanges();
+                var Results = await _dbContext.Players.FindAsync(id);
+                return (Ok(Results));
+            }
+            else
+            {
+                throw new Exception("Invalid value for position: " + position);
+            }
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+
+        //throw new NotImplementedException();
     }
 
     [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteAsync()
+    public async Task<IActionResult> DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            Player player = await _dbContext.Players.FindAsync(id);
+            _dbContext.Players.Attach(player);
+            _dbContext.Players.Remove(player);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+        //throw new NotImplementedException();
     }
 }
